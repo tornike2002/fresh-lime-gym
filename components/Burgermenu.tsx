142a466c789123b19bc8376b9227fe9661b09046
tornike2002 <toko.toka2000@gmail.com>
2024-09-27 19:29:46 +0400

    created burgermenu mobile design

--- components/Burgermenu.tsx
+++ components/Burgermenu.tsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
+import logo from "../assets/home/logo.png";
+import { HiBars3 } from "react-icons/hi2";
+import Link from "next/link";
 const Burgermenu = () => {
-  return <div>Burgermenu</div>;
+  return (
+    <nav className="flex cursor-pointer bg-light-lime justify-between items-center px-6 py-4">
+      <Link href="/">
+        <Image src={logo} alt="logo" width={95} height={28} />
+      </Link>
+      <HiBars3 className="text-2xl cursor-pointer" />
+    </nav>
+  );
 };
 
 export default Burgermenu;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundColor: {
+        "light-lime": "#e6f2ef",
+      },
     },
   },
   plugins: [],
