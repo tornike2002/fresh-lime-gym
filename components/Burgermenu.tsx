211a0f011a19ac9f17694babc3f3918053a0e3d3
tornike2002 <toko.toka2000@gmail.com>
2024-09-27 19:40:43 +0400

    added new font bg color and links

--- components/Burgermenu.tsx
+++ components/Burgermenu.tsx
@@ -1,15 +1,27 @@
+"use client";
+
 import Image from "next/image";
 import logo from "../assets/home/logo.png";
 import { HiBars3 } from "react-icons/hi2";
 import Link from "next/link";
 const Burgermenu = () => {
   return (
-    <nav className="flex cursor-pointer bg-light-lime justify-between items-center px-6 py-4">
-      <Link href="/">
-        <Image src={logo} alt="logo" width={95} height={28} />
-      </Link>
-      <HiBars3 className="text-2xl cursor-pointer" />
-    </nav>
+    <section>
+      <nav className="flex cursor-pointer bg-light-lime justify-between items-center px-6 py-4">
+        <Link href="/">
+          <Image src={logo} alt="logo" width={95} height={28} />
+        </Link>
+        <HiBars3 className="text-2xl cursor-pointer" />
+      </nav>
+      <div className="flex flex-col justify-center items-center 
+      gap-5 text-light-grey font-medium text-xl font-Rubik">
+        <Link href="#">How it works</Link>
+        <Link href="#">Inside us</Link>
+        <Link href="#">Testimonials</Link>
+        <Link href="#">Pricing</Link>
+        <Link href="#">Try for free</Link>
+      </div>
+    </section>
   );
 };
 
