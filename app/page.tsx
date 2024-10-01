0a03855fc30ca4a71c4ad0bcf1e79553941157c2
tornike2002 <toko.toka2000@gmail.com>
2024-10-01 19:21:48 +0400

    added new assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import FeaturedHello from "@/components/featured/FeaturedHello";
 import Hero from "@/components/hero/Hero";
+import HowWeWork from "@/components/howWeWork/HowWeWork";
 
 export default function Home() {
   return (
     <section>
       <Hero />
       <FeaturedHello />
+      <HowWeWork />
     </section>
   );
 }
--- components/howWeWork/HowWeWork.tsx
+++ components/howWeWork/HowWeWork.tsx
@@ -3,7 +3,10 @@ import BenefitCard from "./WorkCards/BenefitCard";
 
 const HowWeWork = () => {
   return (
-    <section>
+    <section className="px-6">
+      <h1 className="text-[#077252] font-Rubik font-medium text-[12.8px]">
+        HOW WE WORK
+      </h1>
       <BenefitCard />
     </section>
   );
