80e8866b5c508195a1047a94fa07a988b13ac868
tornike2002 <toko.toka2000@gmail.com>
2024-10-01 19:13:08 +0400

    mobile featured is done

--- components/featured/FeaturedHello.tsx
+++ components/featured/FeaturedHello.tsx
@@ -1,7 +1,49 @@
 import React from "react";
+import Image from "next/image";
+import tech from "../../assets/featured/techcrunch.png";
+import buss from "../../assets/featured/business-insider.png";
+import newY from "../../assets/featured/the-new-york-times.png";
+import forbes from "../../assets/featured/forbes.png";
+import usa from "../../assets/featured/usa-today.png";
 
 const FeaturedHello = () => {
-  return <div>FeaturedHello</div>;
+  return (
+    <section className="pt-[38px] pb-[76px]">
+      <h2
+        className="text-center font-Rubik text-[11px]
+       text-[#999] font-medium pb-[19px]"
+      >
+        As featured in hello
+      </h2>
+      <div className="flex items-center gap-3 justify-center w-full">
+        <Image
+          src={tech}
+          alt="img"
+          className="w-[15%] max-w-[150px] h-auto filter grayscale"
+        />
+        <Image
+          src={buss}
+          alt="img"
+          className="w-[15%] max-w-[150px] h-auto filter grayscale"
+        />
+        <Image
+          src={newY}
+          alt="img"
+          className="w-[15%] max-w-[150px] h-auto filter grayscale"
+        />
+        <Image
+          src={forbes}
+          alt="img"
+          className="w-[15%] max-w-[150px] h-auto filter grayscale"
+        />
+        <Image
+          src={usa}
+          alt="img"
+          className="w-[15%] max-w-[150px] h-auto filter grayscale"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default FeaturedHello;
