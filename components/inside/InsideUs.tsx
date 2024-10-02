110673fa60e9e98d42be25f17cffb87d4963fc8a
tornike2002 <toko.toka2000@gmail.com>
2024-10-02 18:42:13 +0400

    created new page insideus

---
+++ components/inside/InsideUs.tsx
@@ -0,0 +1,10 @@
+const InsideUs = () => {
+  return (
+    <section>
+      <h3>Inside Us</h3>
+      <p>At our gym u will find everything u need</p>
+    </section>
+  );
+};
+
+export default InsideUs;
