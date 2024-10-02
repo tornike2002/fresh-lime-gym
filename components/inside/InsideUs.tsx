9aa579a6c0bbdc6d093a22e2fc77f320419523c4
tornike2002 <toko.toka2000@gmail.com>
2024-10-02 18:44:15 +0400

    added title styling for that section

--- components/inside/InsideUs.tsx
+++ components/inside/InsideUs.tsx
@@ -1,8 +1,14 @@
 const InsideUs = () => {
   return (
     <section>
-      <h3>Inside Us</h3>
-      <p>At our gym u will find everything u need</p>
+      <h3 className="text-[#077252] mt-20 text-center font-Rubik font-medium text-[12.8px]">
+        Inside Us
+      </h3>
+      <p className="text-center pb-3 pt-3 font-Rubik 
+      text-[26.3px] text-[#333] font-bold tracking-[-0.5px] 
+      leading-[1]">
+        At our gym u will find everything u need
+      </p>
     </section>
   );
 };
