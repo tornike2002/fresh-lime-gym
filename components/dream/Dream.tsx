f7a58e393f4f5f749a0b24cd1249ed55b0a9102a
tornike2002 <toko.toka2000@gmail.com>
2024-10-03 17:20:56 +0400

    first card of dream is done for mobile

--- components/dream/Dream.tsx
+++ components/dream/Dream.tsx
@@ -1,9 +1,19 @@
-
+import DreamCardMontly from "./DreamCardMontly";
 
 const Dream = () => {
   return (
-    <div>Dream</div>
-  )
-}
+    <section className="px-6">
+      <h1 className="pt-[75px] pb-1 text-[#077252] font-Rubik font-medium text-[12.8px]">
+        PRICING
+      </h1>
+      <h1 className="pb-3 font-Rubik text-[26.3px] text-[#333] font-bold tracking-[-0.5px] leading-[1]">
+        Get your dream body
+      </h1>
+      <div>
+        <DreamCardMontly />
+      </div>
+    </section>
+  );
+};
 
-export default Dream
+export default Dream;
