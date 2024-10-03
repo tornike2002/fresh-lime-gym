538865f0a7bd8ea246b303f7bd24aff8b773ff03
tornike2002 <toko.toka2000@gmail.com>
2024-10-03 16:39:55 +0400

    finished mobile styling of benefit reusable card

--- components/benefits/BenefitSkillsCard.tsx
+++ components/benefits/BenefitSkillsCard.tsx
@@ -1,7 +1,24 @@
-import Image from "next/image";
-
-const BenefitSkillsCard = () => {
-  return <div>BenefitSkillsCard</div>;
+import Image, { StaticImageData } from "next/image";
+type benefitTypes = {
+  img: string | StaticImageData;
+  desc: string;
+  name: string;
+};
+const BenefitSkillsCard = ({ img, desc, name }: benefitTypes) => {
+  return (
+    <div>
+      <div className="font-Rubik flex flex-col gap-2 pb-[75px] pt-[25px]">
+        <Image
+          src={img}
+          alt="img"
+          width={51}
+          className="rounded-full"
+        />
+        <p className="text-[#555] text-[14.4px] leading-[1.8]">{desc}</p>
+        <p className="text-[12.8px] text-[#777]">{name}</p>
+      </div>
+    </div>
+  );
 };
 
 export default BenefitSkillsCard;
--- components/benefits/Benefits.tsx
+++ components/benefits/Benefits.tsx
@@ -1,9 +1,21 @@
 import BenefitSkillsCard from "./BenefitSkillsCard";
-
+import profile1 from "../../assets/benefits/customer-1.jpg";
 const Benefits = () => {
   return (
     <section className="bg-[#e6f2ef] px-6">
-      <BenefitSkillsCard />
+      <h1 className="pt-[75px] pb-5 text-[#077252] font-Rubik font-medium text-[12.8px]">
+        HOW WE WORK
+      </h1>
+      <h1 className="pb-3 font-Rubik text-[26.3px] text-[#333] font-bold tracking-[-0.5px] leading-[1]">
+        Our benefits and skills that make u happy
+      </h1>
+      <BenefitSkillsCard
+        img={profile1}
+        desc={
+          "Hi there lola here, I never liked gym before. I am lazy hahahaha, but in Fresh Lime you just can't be lazy, wanna work hours and never stop!"
+        }
+        name={"— Lola Lee"}
+      />
     </section>
   );
 };
