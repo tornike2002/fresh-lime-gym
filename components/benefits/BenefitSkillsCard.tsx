976e389688783a07e3888ee01be6265d85de5250
tornike2002 <toko.toka2000@gmail.com>
2024-10-03 16:21:35 +0400

    created new component and reusable card component for benefit component

---
+++ components/benefits/BenefitSkillsCard.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+const BenefitSkillsCard = () => {
+  return <div>BenefitSkillsCard</div>;
+};
+
+export default BenefitSkillsCard;
---
+++ components/benefits/Benefits.tsx
@@ -0,0 +1,11 @@
+import BenefitSkillsCard from "./BenefitSkillsCard";
+
+const Benefits = () => {
+  return (
+    <section className="bg-[#e6f2ef] px-6">
+      <BenefitSkillsCard />
+    </section>
+  );
+};
+
+export default Benefits;
