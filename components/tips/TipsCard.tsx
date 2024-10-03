b7274a35c2d0331f881f5ec591f509d58f8a6693
tornike2002 <toko.toka2000@gmail.com>
2024-10-03 18:06:39 +0400

    tips cards for mobile is done

--- components/tips/TipsCard.tsx
+++ components/tips/TipsCard.tsx
@@ -1,14 +1,14 @@
 import React from "react";
+type tipsTypes = {
+  title: string;
+  description: string;
+};
 
-const TipsCard = () => {
+const TipsCard = ({ title, description }: tipsTypes) => {
   return (
     <div className="font-Rubik">
-      <h2 className="text-[19.2px] text-[#333] font-bold">
-        Traning every day!
-      </h2>
-      <p className="text-[14.2px] text-[#555] leading-[1.8]">
-        Our gym is open 365 days in year, even holidays you can come!
-      </p>
+      <h2 className="text-[19.2px] text-[#333] font-bold">{title}</h2>
+      <p className="text-[14.2px] text-[#555] leading-[1.8]">{description}</p>
     </div>
   );
 };
